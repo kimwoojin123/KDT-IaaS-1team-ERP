bb0f190ccfb7836d7d32e12d5ce2cfe1ae9b2b57
csm <csm009177@gmail.com>
2023-12-28 11:45:19 +0900

    read json file and print

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello')
+import http from "http";
+import fs, { writeFile } from "fs";
+import { parse, stringify } from "querystring";
+const PORT = 3213;
+const htmlpath = "index.html";
+const JSONPath = "index.json";
+
+const serv = http.createServer((req, res)=>{
+  if(req.method==="GET" && req.url==="/"){
+    fs.readFileSync(htmlpath, 'utf8')
+    const parsedJSON = JSON.parse(fs.readFileSync(JSONPath, 'utf8'))
+    console.log(parsedJSON)
+  }
+});
+
+serv.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}/`);
+});
