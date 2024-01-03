d07cd9d89b9c6c8ecb918ca116ed43af005a0064
JungYoungSick <junfas1216@gmail.com>
2024-01-03 16:44:40 +0900

    다시 설정

---
+++ jung/app/list/page.tsx
@@ -0,0 +1,5 @@
+export default function ProductList() {
+  console.log("ProductList");
+
+  return <li className="w-60 h-60 bg-cyan-600 ">2</li>;
+}
---
+++ jung/app/user/page.tsx
@@ -0,0 +1,5 @@
+export default function AdminManagement() {
+  console.log("AdminManagement");
+
+  return <li className="w-60 h-60 bg-cyan-900">3</li>;
+}
