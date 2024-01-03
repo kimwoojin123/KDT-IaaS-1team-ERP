b52e9257242721d7559c4dc57cb34b939bef4edc
승희 <bangseunghee1108@gmail.com>
2024-01-03 17:38:41 +0900

    오타 수정, next 연결 확인 완료

--- bang/app/page.tsx
+++ bang/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home () {
   return (
     <>
     <div className='box'><Link href="/list">List</Link></div>
-          <div className='box'><Link href="/registeration">Registration</Link></div>
+          <div className='box'><Link href="/registration">Registration</Link></div>
           <div className='box'><Link href="/management">Management</Link></div>
           <div className='box'><Link href="/usermanagement">User-management</Link></div>
     </>
