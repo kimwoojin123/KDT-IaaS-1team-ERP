4eb7579801e1444928c770835adebbd933e67151
승희 <bangseunghee1108@gmail.com>
2024-01-04 13:08:44 +0900

    db연결

--- bang/app.js
+++ bang/app.js
@@ -1,60 +1,54 @@
 
 const express = require("express");
 const next = require('next');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const PORT = 3000;
+const bodyParser = require('body-parser');
 
 const isDev = process.env.NODE_ENV !== 'development';
 const app = next({ dev: isDev });
-// const handle = app.getRequestHandler();
-
+const handle = app.getRequestHandler();
 
 // MariaDB 연결 설정
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: "localhost",
-  user: "admin ",  // 테이블 이름
+  port: '3306',
+  user: "root",
   password: "1108",
-  database: "erpproject",  // 데이터베이스 이름
+  database: "erpproject",
+  connectionLimit : 5,
 });
+
+pool
+  .getConnection()
+  .then((conn) => {
+    console.log("데이터베이스 연결 성공");
+    conn.release();
+  })
+  .catch((err) => {
+    console.error("데이터베이스 연결 실패:", err.message);
+  });
 
 app.prepare().then(() => {
   const server = express();
-  server.use(express.json());
+  server.use(bodyParser.json());
   server.use(express.urlencoded({ extended: true }));
-
-server.get("/product", (req, res) => {
-  const { cateName } = req.query;
-  let query = "SELECT productName FROM product";
-  let params = [];
-
-  if (cateName) {
-    query += " WHERE cateName = ?";
-    params = [cateName];
-  }
   
-  connection.query(query, params, (err, results, fields) => {
-    if (err) {
-      console.error("Error fetching products by category:", err);
-      res.status(500).json({ message: "상품을 불러오는 중에 오류가 발생했습니다." });
-      return;
+  server.get("/api/admins", async (req, res) => {
+    try {
+      const users = await pool.query("SELECT * FROM admin");
+      console.log(users);
+    res.json(users); 
+    } catch (error) {
+      console.error("Error fetching classrooms:", error);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-    
-    res.status(200).json(results);
   });
-});
 
-server.get("/category", (req, res) => {
-  const query = "SELECT cateName FROM category"; // 쿼리로 상품 이름 가져오기
-  connection.query(query, (err, results, fields) => {
-    if (err) {
-      console.error("Error fetching category:", err);
-      res.status(500).json({ message: "카테고리를 불러오는 중에 오류가 발생했습니다." });
-      return;
-    }
-    
-    res.status(200).json(results); // 결과를 JSON 형태로 반환
+  // 기본적인 Next.js 페이지 핸들링
+  server.get("*", (req, res) => {
+    return handle(req, res);
   });
-});
 
 server.listen(3000, (err) => {
   if (err) throw err;
