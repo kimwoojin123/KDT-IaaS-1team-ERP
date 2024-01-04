8f139856880eba8a21590d473676b1f510b33bd1
kimwoojin <kimwoojin1903@gmail.com>
2024-01-04 09:53:09 +0900

    경로수정

---
+++ user/app/productDetail/purchase/page.tsx
@@ -0,0 +1,5 @@
+export default function Purchase(){
+  return (
+    <div>구매페이지</div>
+  )
+}
--- user/app/ui/purchaseButton.tsx
+++ user/app/ui/purchaseButton.tsx
@@ -2,6 +2,6 @@ import Link from 'next/link'
 
 export default function PurchaseButton(){
   return (
-    <Link className="flex justify-center items-center bg-gray-300 w-10 h-7" href="purchase">구매</Link>
+    <Link className="flex justify-center items-center bg-gray-300 w-10 h-7" href="productDetail/purchase">구매</Link>
   )
 }
