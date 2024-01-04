63cb94a4dae5f1eb0ce00fcf995870c8cb97afa8
kimwoojin <kimwoojin1903@gmail.com>
2024-01-04 11:26:59 +0900

    input에 name추가

--- user/app/productDetail/purchase/page.tsx
+++ user/app/productDetail/purchase/page.tsx
@@ -15,27 +15,31 @@ export default function Purchase(){
       setProductPrice(params.price);
     }
   }, [searchParams]);
+
+
+
   return (
     <div className='flex flex-col justify-center items-center w-lvw h-lvh'>
-      <h1>ORDER</h1>
+      <h1 className='text-2xl font-bold'>ORDER</h1>
       <p>배송정보</p>
       <form>
         <li className="flex flex-col w-80">
           <label htmlFor="customer">주문고객</label>
-          <input className='border border-black' type='text' id="customer" />
+          <input className='border border-black' type='text' name='customer' id="customer" />
         </li>
         <li className="flex flex-col w-80">
           <label htmlFor="receiver">받는 분</label>
-          <input className='border border-black' type='text' id="receiver" />
+          <input className='border border-black' type='text' name='receiver' id="receiver" />
         </li> 
         <li className="flex flex-col w-80">
           <label htmlFor="phone">휴대폰번호</label>
-          <input className='border border-black' type='text' id="phone" />
+          <input className='border border-black' type='text' name='phoneNumber' id="phone" />
         </li> 
         <li className="flex flex-col w-80">
           <label htmlFor="address">배송주소</label>
-          <input className='border border-black' type='text' id="adress" />
+          <input className='border border-black' type='text' name='address' id="adress" />
         </li><br />
+        <input type='hidden' name='price' value={productPrice} />
         <p>상품명 : {productName}</p>
         <p>상품 가격: {productPrice}원</p><br />
         <button className="bg-gray-300 w-20 h-10"type="submit">결제하기</button>
