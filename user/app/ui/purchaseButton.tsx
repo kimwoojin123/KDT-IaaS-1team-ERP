ffe4d7fb9e82070105e68b6e113d584c9d465e6a
kimwoojin <kimwoojin1903@gmail.com>
2024-01-04 14:51:35 +0900

    구매버튼에 로그인 필요 추가

--- user/app/ui/purchaseButton.tsx
+++ user/app/ui/purchaseButton.tsx
@@ -1,9 +1,23 @@
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function PurchaseButton({ productName, price }: { productName: string, price: string }){
-  const href = `/productDetail/purchase?productName=${encodeURIComponent(productName)}&price=${encodeURIComponent(price)}`;
+  const router = useRouter();
+
+  const handlePurchaseClick = () => {
+    const token = localStorage.getItem('token');
+
+    if (token) {
+      router.push(`/productDetail/purchase?productName=${encodeURIComponent(productName)}&price=${encodeURIComponent(price)}`);
+    } else {
+      alert('로그인이 필요합니다.');
+    }
+  };
+
 
   return (
-    <Link className="flex justify-center items-center bg-gray-300 w-10 h-7" href={href}>구매</Link>
+    <div className="flex justify-center items-center bg-gray-300 w-10 h-7" >
+    <button onClick={handlePurchaseClick}>구매</button>
+    </div>
   )
 }
