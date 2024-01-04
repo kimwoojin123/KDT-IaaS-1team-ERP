8c355fec32fb32201b582737580d6390e69241cc
kimwoojin <kimwoojin1903@gmail.com>
2024-01-04 19:30:15 +0900

    주문 정보 페이지 세팅

--- user/app/mypage/page.tsx
+++ user/app/mypage/page.tsx
@@ -1,10 +1,12 @@
-import ResignButton from "../ui/mypage/resignbutton"
-import UserInfo from "../ui/mypage/usernameinfo"
+import ResignButton from "../ui/mypage/resignButton"
+import UserInfo from "../ui/mypage/usernameInfo"
+import OrderInfoButton from "../ui/mypage/orderInfoButton"
 
 export default function MyPage(){
   return (
     <div className="flex flex-col w-lvw h-lvh justify-center items-center">
       <UserInfo />
+      <OrderInfoButton />
       <ResignButton />
     </div>
   )
