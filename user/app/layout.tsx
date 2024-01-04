c6b9e7db17d24c860ac6d9cd2bd611836ddbcd42
kimwoojin <kimwoojin1903@gmail.com>
2024-01-04 16:33:41 +0900

    장바구니 버튼 레이아웃 추가

--- user/app/layout.tsx
+++ user/app/layout.tsx
@@ -4,8 +4,9 @@ import { Inter } from 'next/font/google'
 import '@/app/ui/globals.css';
 import Reload from './ui/layoutButtons/reloadButton'
 import LoginButton from './ui/layoutButtons/loginButton'
-import MyPageButton from './ui/mypage/mypagebutton';
+import MyPageButton from './ui/layoutButtons/mypagebutton';
 import SignupButton from './ui/layoutButtons/signupButton';
+import CartButton from './ui/layoutButtons/cartButton';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +25,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className='flex w-lvw h-20 items-center pl-10'> 
             <Reload />
-        <div className='w-64 flex justify-between relative left-3/4'>
+        <div className='w-80 flex absolute left-3/4 justify-between'>
+          <div>
+            <CartButton />
+          </div>
           <div>
             <MyPageButton />
           </div>            
