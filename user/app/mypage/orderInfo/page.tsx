3dd174253312aad3fa39f84cb66bffe315969588
방승희 <bangseunghee@bangseunghee-seunghee.local>
2024-01-09 10:07:21 +0900

    주문목록 페이지 orderKey 삭제

--- user/app/mypage/orderInfo/page.tsx
+++ user/app/mypage/orderInfo/page.tsx
@@ -1,41 +1,8 @@
-// export default function OrderInfo(){
-//   const [oders, setOrders] = useState<Order[]>([]);
-//   useEffect(() => {
-  //     // 사용자명을 어떻게 가져오는지에 대한 로직이 필요합니다.
-  //     const fetchOrders = async () => {
-  
-  //     // 사용자명이 없으면 에러를 출력하고 함수를 종료합니다.
-  //     if (!username) {
-  //       console.error('사용자명을 찾을 수 없습니다.');
-  //       return;
-  //     }
-  
-  //     fetch(`/orderInfo?username=${username}`)
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error('주문 목록을 가져오는데 실패했습니다.');
-  //         }
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         setOrders(data);
-  //       })
-  //       .catch((error) => {
-  //         console.error('Error fetching user orders:', error);
-  //       });
-  //   }, []); 
-//   return(
-//     <div>주문정보</div>
-//   )
-// }
-
-
 import React, { useState, useEffect } from 'react';
 
 
 // 주문 목록을 나타내는 인터페이스
 interface Order {
-  orderKey: number;
   username: string;
   customer: string;
   receiver: string;
@@ -84,7 +51,6 @@ const OrderList: React.FC = () => {
         <tbody>
           {orders.map((order) => (
             <tr key={order.orderKey}>
-              <td>{order.orderKey}</td>
               <td>{order.username}</td>
               <td>{order.customer}</td>
               <td>{order.receiver}</td>
