df84601aa99db6439daf21fb023f2a2a1fa7dd03
csm <csm009177@gmail.com>
2024-01-10 10:59:27 +0900

    link findIDPW page

--- user/app/login/page.tsx
+++ user/app/login/page.tsx
@@ -48,8 +48,8 @@ export default function Login(){
       <form  className = "h-32 flex flex-col items-end justify-around" onSubmit={handleLogin}>
       <input className="border border-black" type="text" value={username} placeholder="아이디" onChange={(e)=>setUsername(e.target.value)}/>
       <input className="border border-black" type="text" value={password} placeholder="비밀번호"  onChange={(e) => setPassword(e.target.value)} />
-      <div className="flex-row ">
-        <Link className="mt-10" href="/findUserInfo">id/pw찾기</Link>
+      <div className="flex-row space-x-0">
+        <Link className="mt-10" href="/login/findUserInfo">id/pw찾기</Link>
         <button className="border border-black" type="submit" >로그인</button>
       </div>
       </form>
