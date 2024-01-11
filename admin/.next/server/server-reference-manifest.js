bed1a76f6ebff9db72e6d7656d3830a672958988
bangseunghee <bangseunghee1108@gmail.com>
2024-01-11 10:57:45 +0900

    .next 에러

--- admin/.next/server/server-reference-manifest.js
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"zYRiOG//uxULe09HTlu4fVTc7hrwgguFH5p25NGO+TA=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"UXrQOEa6Tj79cKiBUFtcCUT9jbT2Yan2WOCUMtMTyFQ=\"\n}"
->>>>>>> origin
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"U6rinJup/Je6I/9kXRsnHiihq5HOM3DE4dJMdCmIa2A=\"\n}"
->>>>>>> origin/work
