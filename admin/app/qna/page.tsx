fadf90556a80db83e2dd9a9061762014f5fa5d8f
bangseunghee <bangseunghee1108@gmail.com>
2024-01-11 11:52:02 +0900

    고객문의 클릭하면 화면에 뜨는지 테스트 확인

---
+++ admin/app/qna/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import React, { useState, useEffect, useCallback, ChangeEvent } from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>안녕하세요</h1>
+      </div>
+      )
+}
