ed85194d4ea0dec13b0685a5340c66e87d93122e
csm <csm009177@gmail.com>
2024-01-12 17:52:53 +0900

    refresh

--- user/app/ui/mypage/orderInfoButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function OrderInfoButton() {
-  return (
-    <Link href="/mypage/orderInfo">
-        <span className="hover:underline">주문 정보</span>
-    </Link>
-  );
-}
