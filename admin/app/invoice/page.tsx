65412122fd0d031979c005c7a38ec4e36a9c76d7
bangseunghee <bangseunghee1108@gmail.com>
2024-01-13 01:30:42 +0900

    css작업 - 색깔, 색깔

--- admin/app/invoice/page.tsx
+++ admin/app/invoice/page.tsx
@@ -30,36 +30,33 @@ export default function Invoice(){
         console.error('Error fetching order:', error);
       });
   }, []);
-
   return (
-    <div className='flex flex-col justify-center items-center w-lvw h-lvh'>
-      <h1 className='font-bold text-2xl'>사용자 목록</h1><br />
-      <table>
-        <thead>
-          <tr className='border border-black'>
-            <th className='border-r border-black'>ID</th>
-            <th className='border-r border-black'>상품명</th>
-            <th className='border-r border-black'>주문자명</th>
-            <th className='border-r border-black'>받는사람</th>
-            <th className='border-r border-black'>휴대폰번호</th>
-            <th className='border-r border-black'>주소</th>
-            <th className='border-r border-black'>가격</th>
+    <div className="relative mx-4 md:mx-8">
+    <h1 className="text-4xl font-bold mb-4">주문 목록</h1>
+    <table className="mt-4 border-collapse border w-full">
+        <thead className="w-full md:w-full mx-auto mt-4 md:mt-8 border-solid border-2">
+          <tr className='text-lg md:text-xl bg-gray-200'>
+            <th className='border-r p-2 text-2xl font-bold text-center'>상품명</th>
+            <th className='border-r p-2 text-2xl font-bold text-center'>주문자명</th>
+            <th className='border-r p-2 text-2xl font-bold text-center'>받는사람</th>
+            <th className='border-r p-2 text-2xl font-bold text-center'>휴대폰번호</th>
+            <th className='border-r p-2 text-2xl font-bold text-center'>주소</th>
+            <th className='border-r p-2 text-2xl font-bold text-center'>가격</th>
           </tr>
         </thead>
         <tbody>
           {orders.map((order, index) => (
-            <tr className = 'border-l border-r border-b border-black'key={index}>
-              <td className='border-r border-black'>{order.username}</td>
-              <td className='border-r border-black'>{order.productName}</td>
-              <td className='border-r border-black'>{order.customer}</td>
-              <td className='border-r border-black'>{order.receiver}</td>
-              <td className='border-r border-black'>{order.phoneNumber}</td>
-              <td className='border-r border-black'>{order.address}</td>
-              <td className='border-r border-black'>{order.price}</td>
+            <tr key={index} className='border-l border-r border-b border-grey-200'>
+              <td className='border-r p-2 text-base text-center'>{order.username}</td>
+              <td className='border-r p-2 text-base text-center'>{order.customer}</td>
+              <td className='border-r p-2 text-base text-center'>{order.receiver}</td>
+              <td className='border-r p-2 text-base text-center'>{order.phoneNumber}</td>
+              <td className='border-r p-2 text-base text-center'>{order.address}</td>
+              <td className='border-r p-2 text-base text-center'>{order.price}</td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
-  )
-}
+  );
+          }  
