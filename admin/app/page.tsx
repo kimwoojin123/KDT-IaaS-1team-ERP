3d9b01f88a86de17dfc114f539cef87fc93aa880
bangseunghee <bangseunghee1108@gmail.com>
2024-01-15 16:26:50 +0900

    mainpage 카테고리 부분 일렬로 수정

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,23 +1,22 @@
-import Link from 'next/link'
-import ApplyButton from './ui/mainButtons/applyButton'
-import InvoiceButton from './ui/mainButtons/invoiceButton'
-import ListButton from './ui/mainButtons/listButton'
-import ManageButton from './ui/mainButtons/manageButton'
-import QnaButton from './ui/mainButtons/qnaButton'
+import Link from 'next/link';
+import ApplyButton from './ui/mainButtons/applyButton';
+import InvoiceButton from './ui/mainButtons/invoiceButton';
+import ListButton from './ui/mainButtons/listButton';
+import ManageButton from './ui/mainButtons/manageButton';
+import QnaButton from './ui/mainButtons/qnaButton';
 
-
-export default function Home(){
+export default function Home() {
   return (
     <div className='flex flex-col justify-center items-center w-lvw h-lvh'>
-      <div className='flex w-3/4 h-1/2 flex-wrap justify-around items-center'>
+      <div className='flex w-3/4 h-1/2 justify-around items-center'>
         <ListButton />
         <ApplyButton />
-      </div>
-      <div className='flex w-3/4 h-1/2 flex-wrap justify-around'>
         <ManageButton />
         <InvoiceButton />
         <QnaButton />
       </div>
+      <div className='flex w-3/4 h-1/2 flex-wrap justify-around'>
+      </div>
     </div>
-  )
+  );
 }
