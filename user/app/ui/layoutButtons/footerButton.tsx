4643ce5a153b8364cbe552c77140e9e86ba2a592
bangseunghee <bangseunghee1108@gmail.com>
2024-01-15 14:00:59 +0900

    footer css 작업

--- user/app/ui/layoutButtons/footerButton.tsx
+++ user/app/ui/layoutButtons/footerButton.tsx
@@ -1,24 +1,23 @@
-// FooterButton.js
+'use client'
 
 import React from 'react';
 
 const FooterButton = () => {
-  // 실제 회사 정보로 덮어쓰기 전에 더미 정보를 실제 정보로 교체할 수 있습니다.
   const companyInfo = {
-    name: '회사명',
+    name: 'DYABYA',
     phoneNumber: '010-1234-5678',
-    email: 'info@company.com',
-    address: '서울특별시 강남구 삼성동 123번지',
+    email: 'DYABYA@green.com',
+    address: '대전광역시 서구 대덕대로 182 오라클빌딩 305호',
     businessNumber: '123-45-67890',
   };
 
   return (
-    <div className="footer-button-container">
-      <h2>{companyInfo.name}</h2>
-      <p>전화번호: {companyInfo.phoneNumber}</p>
-      <p>이메일: {companyInfo.email}</p>
-      <p>주소: {companyInfo.address}</p>
-      <p>사업자 등록 번호: {companyInfo.businessNumber}</p>
+    <div className="footer-button-container bg-gray-200 p-4 rounded-md">
+      <h2 className="text-lg font-semibold">{companyInfo.name}</h2>
+      <p className="text-sm">전화번호: {companyInfo.phoneNumber}</p>
+      <p className="text-sm">이메일: {companyInfo.email}</p>
+      <p className="text-sm">주소: {companyInfo.address}</p>
+      <p className="text-sm">사업자 등록 번호: {companyInfo.businessNumber}</p>
     </div>
   );
 };
