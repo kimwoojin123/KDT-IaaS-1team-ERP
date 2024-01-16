f068d036f2064c145364cd4ca23813a11771d3b0
kimwoojin <kimwoojin1903@gmail.com>
2024-01-16 22:47:53 +0900

    도너츠 차트 세팅

--- admin/app/ui/totalChart.tsx
+++ admin/app/ui/totalChart.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { useEffect, useState } from 'react';
+import Chart from 'chart.js/auto';
 
-export default function TopProductSection() {
+
+export function TopProductSection() {
   const [mostSoldProduct, setMostSoldProduct] = useState({ productKey: 0, totalQuantity: 0, productName: '', price: 0 });
 
   useEffect(() => {
@@ -32,3 +34,73 @@ export default function TopProductSection() {
     </div>
   );
 }
+
+
+
+export function ProductPreferenceChart() {
+  const [productPreferences, setProductPreferences] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/order/productPreferences');
+
+        if (!response.ok) {
+          throw new Error(`물품 선호도 데이터를 가져오지 못했습니다. 상태: ${response.status}`);
+        }
+
+        const preferencesData = await response.json();
+        setProductPreferences(preferencesData);
+      } catch (error) {
+        console.error("물품 선호도 데이터를 가져오는 중 오류 발생:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    // Canvas 요소 가져오기
+    const ctx = document.getElementById('productPreferencesChart') as HTMLCanvasElement;
+
+    // 주문 통계를 나타내는 도넛츠 차트 생성
+    const doughnutChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+        labels: productPreferences.map((item) => item.name),
+        datasets: [{
+          data: productPreferences.map((item) => item.quantity),
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)',
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+          ],
+          borderWidth: 1,
+        }],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+
+    return () => {
+      // Cleanup when the component unmounts
+      doughnutChart.destroy();
+    };
+  }, [productPreferences]);
+
+  return (
+    <div>
+      <h2>최근 30일 물품 선호도</h2>
+      <canvas id="productPreferencesChart" width="400" height="400"></canvas>
+    </div>
+  );
+};
