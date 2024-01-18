cab48d1b159a813f2221a88e711488d6dac15508
bangseunghee <bangseunghee1108@gmail.com>
2024-01-19 02:57:11 +0900

    home - 스타일, 차트 아래 공간에 div 수정 해야 됨

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -1,24 +1,37 @@
-import Link from 'next/link'
-import ApplyButton from './ui/mainButtons/applyButton'
-import InvoiceButton from './ui/mainButtons/invoiceButton'
-import ListButton from './ui/mainButtons/listButton'
-import ManageButton from './ui/mainButtons/manageButton'
-import QnaButton from './ui/mainButtons/qnaButton'
-import SaleChart from './ui/saleChart'
-import { TopProductSection } from './ui/totalChart'
+import Link from 'next/link';
+import ApplyButton from './ui/mainButtons/applyButton';
+import InvoiceButton from './ui/mainButtons/invoiceButton';
+import ListButton from './ui/mainButtons/listButton';
+import ManageButton from './ui/mainButtons/manageButton';
+import QnaButton from './ui/mainButtons/qnaButton';
+import SaleChart from './ui/saleChart';
+import { TopProductSection } from './ui/totalChart';
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center items-center w-lvw h-lvh'>
-      <div className='flex w-3/4 h-1/2 justify-around items-center'>
+    <div className='flex mt-10'>
+      {/* 카테고리 부분 */}
+      <div className='w-1/6 h-full flex flex-col justify-around items-center mt-8'>
         <ListButton />
         <ApplyButton />
         <ManageButton />
         <InvoiceButton />
         <QnaButton />
       </div>
-      <SaleChart />
-      <TopProductSection />
+
+      {/* 나머지 80%의 공간에 차트와 공지사항 */}
+      <div className='w-5/6 h-full overflow-hidden flex flex-col items-center'>
+        {/* 차트 섹션 */}
+        <div className='w-3/4 h-1/3 mb-4 overflow-hidden'>
+          <SaleChart />
+        </div>
+
+        {/* 공지사항 섹션 또는 다른 내용 추가 가능 */}
+        <div className='w-full h-1/2 overflow-y-auto'>
+          {/* 조정된 크기의 표 */}
+          <TopProductSection />
+        </div>
+      </div>
     </div>
   );
 }
