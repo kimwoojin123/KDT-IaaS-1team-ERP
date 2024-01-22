3ffcd5b363bf6d597f7e5987455a19ecec0d5859
kimwoojin <kimwoojin1903@gmail.com>
2024-01-22 16:24:41 +0900

    회원가입 페이지 css 업데이트

--- user/app/signup/page.tsx
+++ user/app/signup/page.tsx
@@ -193,119 +193,154 @@ export default function SignUp(){
   };
 
   return (
-    <div className="flex flex-col justify-center items-center h-lvh">
-      <h1 className="mb-32">회원가입 페이지</h1>
-      <form
-        className="h-32 flex flex-col items-end justify-around"
-        onSubmit={handleJoin}
-      >
-        <input
-          className={`border border-black mb-2 ${
-            !validation.isValidName ? "border-red-500" : ""
-          }`}
-          type="text"
-          value={formData.name}
-          name="name"
-          placeholder="이름"
-          onChange={handleInputChange}
-        />
-        {!validation.isValidName && (
-          <p style={{ color: "red", fontSize: 10 }}>이름을 확인하세요</p>
-        )}
-        <input
-          className={`border border-black mb-2 ${
-            !validation.isValidUsername ? "border-red-500" : ""
-          }`}
-          type="text"
-          value={formData.username}
-          name="username"
-          placeholder="아이디"
-          onChange={handleInputChange}
-        />
-        {!validation.isValidUsername && (
-          <p style={{ color: "red", fontSize: 10 }}>
-            6~12글자,영문,숫자로 작성하세요(특수문자 제한)
-          </p>
-        )}
-        <button type="button" onClick={checkUsername}>중복조회</button>
-        <input
-          className={`border border-black mb-2 ${
-            !validation.isValidPassword ? "border-red-500" : ""
-          }`}
-          type="password"
-          value={formData.password}
-          name="password"
-          placeholder="비밀번호"
-          onChange={handleInputChange}
-        />
-        {!validation.isValidPassword && (
-          <p style={{ color: "red", fontSize: 10 }}>
-            8~20글자, 영문,숫자,특수문자로 작성하세요
-          </p>
-        )}
-        <input
-        className={`border border-black mb-2 ${
-          !validation.isValidConfirmPassword ? "border-red-500" : ""
-        }`}
-        type="password" // 추가: 비밀번호 필드로 변경
-        value={formData.confirmPassword}
-        name="confirmPassword"
-        placeholder="비밀번호 확인"
-        onChange={handleInputChange}
-      />
-      {!validation.isValidConfirmPassword && (
-        <p style={{ color: "red", fontSize: 10 }}>비밀번호가 일치하지 않습니다</p>
-      )}
-        <input
-          className={`border border-black mb-2 ${
-            !validation.isValidEmail ? "border-red-500" : ""
-          }`}
-          type="text"
-          value={formData.email}
-          name="email"
-          placeholder="이메일"
-          onChange={handleInputChange}
-        />
-        {!validation.isValidEmail && (
-          <p style={{ color: "red", fontSize: 10 }}>
-            이메일을 다시 확인 후 입력해주세요
-          </p>
-        )}
-        <Addr onAddressSelect={handleAddressSelect} />
-        <input
-          className='border border-black mb-2'
-          type="text"
-          value={formData.address}
-          name="address"
-          placeholder="주소"
-          onChange={handleInputChange}
-          readOnly
-        />
-
-        <input
-        className='border border-black mb-2'
-        type="text"
-        value={formData.detailedAddress}
-        name="detailedAddress"
-        placeholder="상세주소"
-        onChange={handleDetailedAddressChange}
-        />
-
-        <input
-          className='border border-black mb-2'
-          type="text"
-          value={formData.phoneNumber}
-          name="phoneNumber"
-          placeholder="전화번호"
-          onChange={handlePhoneNumberChange}
-          required
-        />
-        {!validation.isValidPhoneNumber && (
-          <p style={{ color: "red", fontSize: 10 }}>전화번호가 올바르지 않습니다.</p>
-        )}
-        <button type="submit">회원가입</button>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="mb-10 text-3xl font-bold">회원가입 페이지</h1>
+      <form className="w-full max-w-md" onSubmit={handleJoin}>
+        <div className="mb-6 flex flex-col">
+          <div className="relative mb-4 flex items-center">
+            <div className="w-full">
+              <input
+                className={`w-full border p-2 ${
+                  !validation.isValidName ? "border-red-500" : "border-black"
+                }`}
+                type="text"
+                value={formData.name}
+                name="name"
+                placeholder="이름"
+                onChange={handleInputChange}
+              />
+              {validation.isValidName ? null : (
+                <p className="text-red-500 text-xs mt-1">이름을 확인하세요</p>
+              )}
+            </div>
+          </div>
+          <div className="relative mb-4 flex items-center">
+            <div className="w-full">
+              <input
+                className={`w-full border p-2 ${
+                  !validation.isValidUsername
+                    ? "border-red-500"
+                    : "border-black"
+                }`}
+                type="text"
+                value={formData.username}
+                name="username"
+                placeholder="아이디"
+                onChange={handleInputChange}
+              />
+              {!validation.isValidUsername && (
+                <p className="text-red-500 text-xs mt-1">
+                  6~12글자, 영문, 숫자로 작성하세요 (특수문자 제한)
+                </p>
+              )}
+            </div>
+            <button
+              type="button"
+              onClick={checkUsername}
+              className="bg-blue-500 w-32 h-12 text-white py-0 px-4 rounded ml-2"
+            >
+              중복조회
+            </button>
+          </div>
+          <input
+            className={`w-full border p-2 mb-4 border-black ${
+              !validation.isValidPassword ? "border-red-500" : ""
+            }`}
+            type="password"
+            value={formData.password}
+            name="password"
+            placeholder="비밀번호"
+            onChange={handleInputChange}
+          />
+          {!validation.isValidPassword && (
+            <p style={{ color: "red", fontSize: 10 }}>
+              8~20글자, 영문,숫자,특수문자로 작성하세요
+            </p>
+          )}
+          <input
+            className={`w-full border p-2 mb-4 border-black ${
+              !validation.isValidConfirmPassword ? "border-red-500" : ""
+            }`}
+            type="password" // 추가: 비밀번호 필드로 변경
+            value={formData.confirmPassword}
+            name="confirmPassword"
+            placeholder="비밀번호 확인"
+            onChange={handleInputChange}
+          />
+          {!validation.isValidConfirmPassword && (
+            <p style={{ color: "red", fontSize: 10 }}>
+              비밀번호가 일치하지 않습니다
+            </p>
+          )}
+          <div className="relative mb-4 flex items-center">
+            <div className="w-full">
+              <input
+                className={`w-full border p-2 ${
+                  !validation.isValidEmail ? "border-red-500" : "border-black"
+                }`}
+                type="text"
+                value={formData.email}
+                name="email"
+                placeholder="이메일"
+                onChange={handleInputChange}
+              />
+              {validation.isValidEmail ? null : (
+                <p className="text-red-500 text-xs mt-1">
+                  이메일을 다시 확인 후 입력해주세요
+                </p>
+              )}
+            </div>
+          </div>
+          <input
+            className={`w-full border p-2 ${
+              !validation.isValidPhoneNumber ? "border-red-500" : "border-black"
+            }`}
+            type="text"
+            value={formData.phoneNumber}
+            name="phoneNumber"
+            placeholder="전화번호"
+            onChange={handleInputChange}
+          />
+          <div className="w-full">
+            {validation.isValidPhoneNumber ? null : (
+              <p className="text-red-500 text-xs mt-1">
+                " - "를 사용하여 작성해주세요.
+              </p>
+            )}
+          </div>
+          <div className="relative flex items-center">
+            <div className="w-full">
+              <div className="flex justify-end">
+                <Addr onAddressSelect={handleAddressSelect} />
+              </div>
+              <input
+                className="w-full border p-2 mb-4 border-black"
+                type="text"
+                value={formData.address}
+                name="address"
+                placeholder="주소"
+                onChange={handleInputChange}
+              />
+              <input
+                className="w-full border p-2 border-black"
+                type="text"
+                value={formData.detailedAddress}
+                name="detailedAddress"
+                placeholder="상세주소"
+                onChange={handleDetailedAddressChange}
+              />
+              <div className="relative mb-4 flex items-center"></div>
+            </div>
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="bg-green-500 text-white py-2 px-4 rounded mt-4 w-full"
+        >
+          가입하기
+        </button>
       </form>
-      <Link className="mt-32" href="/login">
+      <Link className="mt-8" href="/login">
         로그인페이지로
       </Link>
     </div>
