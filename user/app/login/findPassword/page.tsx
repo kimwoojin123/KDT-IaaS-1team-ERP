42b2e43c4e0e25f2aed7680e9d2c578037fd523f
kimwoojin <kimwoojin1903@gmail.com>
2024-01-23 13:05:47 +0900

    비밀번호 변경페이지 css / 모달 추가

--- user/app/login/findPassword/page.tsx
+++ user/app/login/findPassword/page.tsx
@@ -1,13 +1,21 @@
 'use client'
 
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 
 const FindPasswordPage = () => {
   const [name, setName] = useState('');
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [newPassword, setNewPassword] = useState('');
-  const [message, setMessage] = useState('');
+  const [error, setError] = useState<string>('');
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
+
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+
 
   const handleFindPassword = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -26,14 +34,17 @@ const FindPasswordPage = () => {
       }
 
       const data = await response.json();
-      setMessage(data.message);
-
+      
       if (data.message === '해당 사용자를 찾았습니다.') {
         // 찾은 사용자가 있을 때 비밀번호 변경 관련 input 표시
         setNewPassword('');
+        setError('');
+        openModal();
       }
     } catch (error) {
       console.error('Error:', error);
+      setError('아이디를 찾을 수 없습니다.\n입력 정보를 확인해주세요.');
+      openModal();
     }
   };
 
@@ -50,20 +61,21 @@ const FindPasswordPage = () => {
       if (!response.ok) {
         throw new Error('서버 오류 발생');
       }
-
-      setMessage('비밀번호가 업데이트되었습니다.');
+      setError('비밀번호가 성공적으로 변경되었습니다.'); // 성공했을 경우 에러 메시지 초기화
     } catch (error) {
       console.error('Error:', error);
+      setError('비밀번호 변경 중 오류가 발생했습니다.');
     }
   };
 
   return (
-    <div>
-      <h1>비밀번호 찾기</h1>
-      <form onSubmit={handleFindPassword}>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1 className="mb-10 text-3xl font-bold">비밀번호 찾기</h1>
+      <form className="w-full max-w-md" onSubmit={handleFindPassword}>
         <label>
-          이름:
+          이름
           <input
+            className="w-full border border-black p-2 mb-2"
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
@@ -71,8 +83,9 @@ const FindPasswordPage = () => {
         </label>
         <br />
         <label>
-          아이디:
+          아이디
           <input
+            className="w-full border border-black p-2 mb-2"
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
@@ -80,19 +93,55 @@ const FindPasswordPage = () => {
         </label>
         <br />
         <label>
-          이메일:
+          이메일
           <input
+            className="w-full border border-black p-2 mb-2"
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <br />
-        <button type="submit">비밀번호 찾기</button>
+        <button 
+        className="border border-black p-2 bg-blue-500 text-white rounded"
+        type="submit">비밀번호 찾기</button>
       </form>
-      {message && <h2>{message}</h2>}
-      {message === '해당 사용자를 찾았습니다.' && (
-        <div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={{
+          overlay: {
+            backgroundColor: " rgba(0, 0, 0, 0.4)",
+            width: "100%",
+            height: "100vh",
+            zIndex: "10",
+            position: "fixed",
+            top: "0",
+            left: "0",
+          },
+          content: {
+            display:"flex",
+            flexDirection : "column",
+            alignItems : 'center',
+            width: "360px",
+            height: "180px",
+            zIndex: "150",
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            borderRadius: "10px",
+            boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+            backgroundColor: "white",
+            justifyContent: "center",
+            overflow: "auto",
+            whiteSpace: 'pre-line',
+          },
+        }}
+        contentLabel="아이디 찾기 모달"
+      >
+        <h2>{error ? '' : `비밀번호 변경`}</h2>
+        <p>{error || (<div>
           <input
             type="password"
             value={newPassword}
@@ -100,8 +149,10 @@ const FindPasswordPage = () => {
             placeholder="새로운 비밀번호 입력"
           />
           <button onClick={handleUpdatePassword}>비밀번호 변경</button>
-        </div>
-      )}
+        </div>)}</p>
+        <button className="w-40 h-10 rounded-2xl bg-gray-200 mt-5"onClick={closeModal}>닫기</button>
+      </Modal>
+
     </div>
   );
 };
