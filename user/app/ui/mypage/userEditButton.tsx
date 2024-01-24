fff9d092cbe26cce036e188549051a610a23e275
csm <csm009177@gmail.com>
2024-01-24 14:32:46 +0900

    refresh

--- user/app/ui/mypage/userEditButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link'
-
-export default function UserEditButton() {
-  return (
-    <Link href="/mypage/userEdit">
-        <span className="hover:underline">회원정보수정</span>
-    </Link>
-  );
-}
